4c84a357877f71c7fa35ec4314eb9ac05e65bb92
mobil <TeXHuK917@gmail.com>
2024-02-21 13:08:11 +0700

    Another commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PR7_Piterskiy_PR-23.107"
+include(":app")
